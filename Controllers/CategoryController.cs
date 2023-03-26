6f10e20645c99ce0fb5aa9da2bd9140170d893bf
Javeria Tabassum <javeria.tabassum145@gmail.com>
2023-03-27 00:30:22 +0500

    updated code

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using CRUDWebApplication.Data;
+using CRUDWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUDWebApplication.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ApplicationDBContext _context;
+
+        public CategoryController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categories = _context.Categories;
+            return View(categories);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,17 @@
+using CRUDWebApplication.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//*Added Context File and add sql server
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+//*
 
 var app = builder.Build();
 
